6e1db757bc0f2afb3284709344a2f7ff2675160e
fanchao <1104641801@qq.com>
2016-10-25 10:58:48 +0800

    Update MyConstant.java

--- src/main/java/com/fc/util/MyConstant.java
+++ src/main/java/com/fc/util/MyConstant.java
@@ -3,10 +3,10 @@ package com.fc.util;
 
 public class MyConstant {
     //七牛云相关
-    public static final String QINIU_IMAGE_URL = "http://od6v5lenq.bkt.clouddn.com/";
-    public static final String QINIU_ACCESS_KEY = "OO_3P93p4-feCji_Vd3SQPA4FtrUpjw8bL1jXhF8";
-    public static final String QINIU_SECRET_KEY = "AXdJXoYtGL42K_w7UzaNxhrNU55K_XEkv-Zgte1w";
-    public static final String QINIU_BUCKET_NAME = "excited";
+    public static final String QINIU_IMAGE_URL = "你的七牛云IMAGE_URL";
+    public static final String QINIU_ACCESS_KEY = "你的七牛云ACCESS_KEY";
+    public static final String QINIU_SECRET_KEY = "你的七牛云SECRET_KEY";
+    public static final String QINIU_BUCKET_NAME = "你的七牛云BUCKET_NAME";
 
     //发送邮件的邮箱，要与df.properties中的一致
     public static final String MAIL_FROM = "fanchao100@yeah.net";
